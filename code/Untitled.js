9db41754f70230ad0ba502c185846b959985e124
Maxell <alberto.manrique99@gmail.com>
2020-12-13 19:43:14 -0400

    descripcion

---
+++ code/Untitled.js
@@ -0,0 +1,5 @@
+document.getElementById("boton").onclick = function(){
+			
+    document.getElementById("text").innerHTML="Diferente a Escribir algo";
+    
+    }
---
+++ code/logica.js
@@ -0,0 +1,4 @@
+document.getElementsById("boton3").onclick =function(){
+						
+    alert("Paso algo");    
+    }

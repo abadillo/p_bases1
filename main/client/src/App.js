7046dcc3e62b8dcd52cfb323a74eddd632f5985d
antonio2924 <abadillo2924@gmail.com>
2020-12-13 01:19:11 -0400

    añadido front end

--- main/client/src/App.js
+++ main/client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React, {Fragment} from "react";
 import './App.css';
 
+//componetnes 
+
+import IngresarCliente_natural from "./components/IngresarCliente_natural";
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <Fragment>
+      <div className= "container">
+         <IngresarCliente_natural />
+      </div>
+
+    </Fragment>
   );
 }
 

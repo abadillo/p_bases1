64270d0c0d827b79045d34afe0c0132ef4596f71
antonio2924 <abadillo2924@gmail.com>
2020-12-13 18:50:08 -0400

    final final?

---
+++ main/final/Business_Management.py
@@ -0,0 +1,29 @@
+
+from DB import DB
+
+class Business_Management(DB):
+
+    def get_id (self):
+        
+        id = 1000
+
+        try:
+            userscursor = self.cursor.child("Business").get()
+
+            for users in userscursor.each():
+                if (int(users.key()) >= id):
+                    id = int(users.key())
+
+            userscursor = self.cursor.child("Client").get()
+
+            for users in userscursor.each():
+                if (int(users.key()) >= id):
+                    id = int(users.key())
+
+        except Exception:
+            print("Vacio")
+
+        return id + 1
+
+
+   
--- main/final/DB.py
+++ main/final/DB.py
@@ -1,46 +1,37 @@
 from abc import abstractmethod, ABCMeta
 import psycopg2
 from psycopg2 import Error
-
+import logging
 
 class DB(metaclass=ABCMeta):
 
     def __init__(self):
         
         try:
-            connection = psycopg2.connect(
+            self.connection = psycopg2.connect(
                 host = "labs-dbservices01.ucab.edu.ve",
+                user = "grupo4bd1",
                 password = "bases1_abgmjd",
-                username = "grupo4bd1",
                 port ="5432",
-                dbname = "grupo4db1_"
+                database = "grupo4db1_"
             )
 
-            self.cursor = connection.cursor()
+            
+            self.cursor = self.connection.cursor()
             print("Conexion Establecida: ")
-            print(connection.get_dsn_parameters(), "\n")
+            print(self.connection.get_dsn_parameters(), "\n")
+
 
         except (Exception, Error) as error:
-            print("Error en Conexion", error)        
-            
+            print("Error en Conexion", error) 
+            self.cursor = None
 
                 
     @abstractmethod
     def get_id (self):
         pass
        
-    @abstractmethod
-    def get( self, id):
-        pass
-
-    @abstractmethod
-    def gets(self):
-        pass
-
-    @abstractmethod
-    def add( self, item , item2 ):
-        pass
-
+   
 
 
    

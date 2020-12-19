1bf21475710d14fd7fc3d26424a5f6e421c87500
antonio2924 <abadillo2924@gmail.com>
2020-12-19 17:44:50 -0400

    19/12

--- main/sql/pysql.py
+++ main/sql/pysql.py
@@ -11,3 +11,29 @@ data = {
     'cl_p_apellido' : 'will',               #string 
     'cl_s_apellido' : 'rex',                #string  #None
 }
+
+​selecttipo1: "on"
+                        ​
+inputcedula: "26993117"            ​
+         ​
+inputcorreo: "alex.1@gmail.com"            ​
+inputpapellido: "leon"            ​
+inputpnombre: "alex"            ​
+inputrif: "G668231212"            ​
+inputsapellido: "alexander"            ​
+inputsnombre: "badillo"      
+inputcont: "123"            ​
+inputcontval: "123"   
+      ​
+inputtelefono: "04125788466"            ​
+inputtelefono2: "04128744123"            ​
+inputtelefono3: "02121299311"  
+        ​
+mediopago1: "on"            ​
+mediopago3: "on"            ​
+
+selectmunicipio: "8"            ​
+selectparroquia: "4"
+selectestado:"2"
+inputapt: "82"            ​
+inputcalle: "Av andres bello 12"  

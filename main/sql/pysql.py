a74c551ebee311ad48f271991327f15608083f91
antonio2924 <abadillo2924@gmail.com>
2020-12-19 17:20:00 -0400

    C:/Program Files/Git/sql

---
+++ main/sql/pysql.py
@@ -0,0 +1,13 @@
+data = {
+    'cl_correo'     : 'alex@gmail.com',     #string
+    'cl_cedula'     : 21522033,             #int 
+    'cl_rif'        : 122121,               #int
+    
+    'cl_contraseña' : 'buenobueno',         #string 
+    'cl_puntos'     : 0,                    #int
+    'cl_afiliacion' : 123,                  #int
+    'cl_p_nombre'   : 'fernan',             #string 
+    'cl_s_nombre'   : 'flow',               #string  #None
+    'cl_p_apellido' : 'will',               #string 
+    'cl_s_apellido' : 'rex',                #string  #None
+}

322a04e81e86a2c21072d5ac1eebf3e4c4c7abdf
antonio2924 <abadillo2924@gmail.com>
2020-12-28 18:00:37 -0400

    lugares

--- main/final/static/js/registro_natural.js
+++ main/final/static/js/registro_natural.js
@@ -22,7 +22,8 @@ $(document).ready(function() {
         }).done(function(resp){
             
             console.log(resp);
-            estados = resp;         
+            estados = resp;          
+            
 
             var opciones = [];
 

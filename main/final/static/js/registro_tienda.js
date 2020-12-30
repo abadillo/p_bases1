7d094f2752a73157bb7a574c7f6c3ac9a9f34922
Maxell <alberto.manrique99@gmail.com>
2020-12-30 16:19:27 -0400

    desd

--- main/final/static/js/registro_tienda.js
+++ main/final/static/js/registro_tienda.js
@@ -1,12 +1,135 @@
+function c_error(mensaje){
+    $(m_invalido).replaceWith( '<p id="m_invalido">'+mensaje+'</p>'   )
+};
+
+
+function lugares (lu_tipo, fk_lugar,sel_op,y){
+
+    $.ajax({
+                
+        url:   '/lugares',
+        type: 'GET',
+        data: {
+            'LUGAR': lu_tipo ,
+            'FK_LUGAR': fk_lugar,
+        },
+        async: false, 
+            
+        }).done(function(resp){
+        
+            lugars = resp;     
+            console.log(lugars);     
+
+            var opciones = [];
+
+            opciones.push('<option value="0" selected disabled>'+lu_tipo+'</option>');
+
+            for (var i=0, l=lugars.length; i<l; i++){
+
+                if (sel_op == lugars[i].lu_codigo)
+                    opciones.push('<option selected value="'+lugars[i].lu_codigo+'">'+lugars[i].lu_nombre+'<opciones>');
+                else 
+                    opciones.push('<option value="'+lugars[i].lu_codigo+'">'+lugars[i].lu_nombre+'<opciones>');
+
+            }
+
+            console.log(y);
+
+            if (y == 1) {
+
+                if (lu_tipo == 'ESTADO')
+                    $('#selectestado').html(opciones.join(''));
+                else if (lu_tipo == 'MUNICIPIO')
+                    $('#selectmunicipio').html(opciones.join(''));
+                else 
+                    $('#selectparroquia').html(opciones.join(''));
+
+            } else if (y == 2){
+                
+                if (lu_tipo == 'ESTADO')
+                    $('#selectestado2').html(opciones.join(''));
+                else if (lu_tipo == 'MUNICIPIO')
+                    $('#selectmunicipio2').html(opciones.join(''));
+                else 
+                    $('#selectparroquia2').html(opciones.join(''));
+                                    
+            }
+            
+
+            
+        }).fail(function(resp){
+            c_error('No se pudo acceder al servidor. Intente de nuevo mas tarde');
+    });
+
+
+};
+
+
 $(document).ready(function() {
+
+    var id_estado;
+    var id_municipio;
+    var id_parroquia;
+
+    lugares('ESTADO','',0,1);
+    
+   
+    $('#selectestado').change(function() {
+
+        id_estado = $(this).find('option:selected').val();
+        lugares('MUNICIPIO',id_estado,0,1);
+    });
+    
+    $('#selectmunicipio').change(function() {
+
+        id_municipio = $(this).find('option:selected').val();
+        lugares('PARROQUIA',id_municipio,0,1);
+    });
+
+    $('#selectparroquia').change(function() {
+
+        id_parroquia = $(this).find('option:selected').val();
+    });
+
+
+
+
+    var id_estado2;
+    var id_municipio2;
+    var id_parroquia2;
+
+    lugares('ESTADO','',0,2);
+    
+    $('#selectestado2').change(function() {
+
+        id_estado2 = $(this).find('option:selected').val();
+        lugares('MUNICIPIO',id_estado2,0,2);
+    });
+    
+    $('#selectmunicipio2').change(function() {
+
+        id_municipio2 = $(this).find('option:selected').val();
+        lugares('PARROQUIA',id_municipio2,0,2);
+    });
+
+    $('#selectparroquia2').change(function() {
+
+        id_parroquia2 = $(this).find('option:selected').val();
+    });
+
+
+
+
+
+
+
 
     var tiendas;
     var id_tienda;
     
-    $.ajax({
-                
+    $.ajax({ 
         url:   '/tiendas',
-        type: 'POST',
+        type: 'GET',
         dataSrc: "",
             
         }).done(function(resp){
@@ -26,26 +149,28 @@ $(document).ready(function() {
 
             
         }).fail(function(resp){
-            $(document).html('<div class="alert alert-danger">No se pudo acceder al servidor. Intente de nuevo mas tarde</div>');
+            c_error('No se pudo acceder al servidor. Intente de nuevo mas tarde');
     });
 
-    
+
     $('#selecttienda').change(function() {
 
         id_tienda = $(this).find('option:selected').val();
         console.log(id_tienda);
 
-    });
+    });    
+
     
+
 });
 
-/*
+
 $(function(){
     $('form').submit(function(e){
 
         $.ajax({
             
-            url:   '/lugares',
+            url:   '/tienda',
             data:  $('form').serialize(),
             type: 'POST',
                 
@@ -53,13 +178,17 @@ $(function(){
                 
                 console.log(response);
                 
-                if (response['mensaje'] == 'correo o contraseña invalida')
-                    $(error).replaceWith( '<p id="error">'+response['mensaje']+'</p>'   )
-                else
-                    window.location.href =  "/"			
+                if(response['error'])
+					c_error(response['error']);
+				
+				else if (response['invalido'])
+                    c_error(response['invalido']);
+				
+				else
+					window.location.href =  "/"				
                 
             }).fail(function(response){
-                $('form').html('<div class="alert alert-danger">No se pudo acceder al servidor. Intente de nuevo mas tarde</div>');
+                c_error('No se pudo acceder al servidor. Intente de nuevo mas tarde');
             });
 
         e.preventDefault();
@@ -68,5 +197,3 @@ $(function(){
 });
 
 
-
-*/

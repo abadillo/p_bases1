1a37bf2dbc400ce6eb5fae916b21db0e20c7f688
antonio2924 <abadillo2924@gmail.com>
2020-12-30 17:26:45 -0400

    CAMBIO APP.PY

--- main/final/app.py
+++ main/final/app.py
@@ -16,10 +16,14 @@ app = Flask(__name__)
 app.debug = True
 
 
-####################################################################
 
 
-@app.route('/')                                     #metodo request de default es GET
+
+
+#### Interfaces principales ####
+
+
+@app.route('/')                               
 def main():
     return render_template('inicio.html')
     
@@ -42,96 +46,187 @@ def inicio_sesion():
 
         return resp
         
-     
-@app.route('/registro_natural', methods= ['GET', 'POST'] )
-def registro_natural():
+
+
+
+
+##### Interfaces de registro/ver perfil #####
+
+
+@app.route('/<accion>/tienda',methods=['GET'])
+def tienda(accion):
+
+    if accion == 'perfil':
+        return render_template("perfil_tienda.html")
     
-    if request.method == 'GET':
+    if accion == 'registro'
+        return render_template("registro_tienda.html")
+    
+
+@app.route('/<accion>/natural',methods=['GET'])
+def natural(accion):
+
+    if accion == 'perfil':
+        return render_template("perfil_natural.html")
+    
+    if accion == 'registro'
         return render_template("registro_natural.html")
     
-    else:
-        try:
-            
-            db = DB_lugar()   
 
-            lu_codigo = db.getlastid()
+@app.route('/<accion>/juridico',methods=['GET'])
+def natural(accion):
 
-            direccion = {
-                'lu_codigo'     :   lu_codigo,
-                'lu_nombre'     :   request.form['inputdir'],        
-                'lu_tipo'       :   'DIRECCION',             
-                'fk_lugar'      :   request.form['selectparroquia'],         
-            }
-
-            id_direccion = db.add(direccion)
-
-
-            data = {
-                'cl_correo'     :    request.form['inputcorreo'],            #string    
-                'cl_cedula'     :int(request.form['inputcedula']),             #int 
-                'cl_rif'        :    request.form['inputrif'],               #int
-                
-                'cl_contrasena' :    request.form['inputcont'],         #string 
-                'cl_afiliacion' :    123,                                       #int
-                'cl_p_nombre'   :    request.form['inputpnombre'],             #string 
-                'cl_s_nombre'   :    request.form['inputsnombre'],               #string 
-                'cl_p_apellido' :    request.form['inputpapellido'],               #string 
-                'cl_s_apellido' :    request.form['inputsapellido'],                #string  
-                'fk_lugar'      :    None,    
-                'fk_tienda'     :    int(request.form['selecttienda']),
-            }
-
-            data['fk_lugar'] = id_direccion
-
-
-            for key in data.keys():
-                if (data[key] == '' or data[key] == ' '): data[key] = None
-
-            db = DB_cliente_natural()   
-            resp = db.add(data)
-
-
-            db = DB_telefono()   
-
-            telefono = {
-                'te_tipo'            :   request.form['tipotlf'],        
-                'te_numero'          :   int(request.form['inputtelefono']),             
-                'fk_cliente_natural' :   None,         
-            }
-
-            db.add(telefono) 
-
-
-            return resp
-
-        except Exception :
-            return jsonify({'error':'Error: Hubo un problema con el servidor'})
-
-
-@app.route('/registro_juridico', methods= ['GET', 'POST'] )
-def registro_juridico():
+    if accion == 'perfil':
+        return render_template("perfil_juridico.html")
     
-    if request.method == 'GET':
+    if accion == 'registro'
         return render_template("registro_juridico.html")
     
-    else:
-        return xd 
 
 
 
-@app.route('/mostrar',methods=['POST','GET','DELETE'])     #datatable falta delete y update
-def mostrar():
+#####  DATATABLE  #####
+
+@app.route('/mostrar/<obj>',methods=['GET'])    
+def mostrar(obj):
+
+    if obj == 'cliente':
     
+        if request.method == 'GET':
+            return render_template("mostrar_clientes.html")
+
+        if request.method == 'POST':
+
+            db = DB_cliente_natural()         
+            resp = db.getall()
+
+            return jsonify(resp)    
+
+
+    if obj == 'lugar':
+
+        if request.method == 'GET':
+
+            return render_template
+    
+        if request.method == 'POST'
+
+            db = DB_tienda()  
+            resp = db.getall()
+            return jsonify(resp)
+
+
+
+
+
+########   REST   ###########
+
+
+@app.route('/manejo_tienda',methods=['GET', 'POST','PUT','DELETE'])
+def manejo_tienda():
+    
+    if request.method == 'GET':                 #busca datos con el ti_codigo
+        return "datos de tienda"
+
+    if request.method == 'POST':                    #crea la tienda con (Data)
+
+        db = DB_lugar()   
+
+        direccion = {
+            'lu_codigo'     :   None,
+            'lu_nombre'     :   request.form['inputdir'],        
+            'lu_tipo'       :   'DIRECCION',             
+            'fk_lugar'      :   request.form['selectparroquia'],         
+        }
+
+        id_direccion = db.add(direccion)
+
+        data = {
+            'ti_nombre'     :   request.form['inputtienda'],                 
+            'fk_lugar'      :   id_direccion,
+        }
+
+        db = DB_tienda()  
+        resp = db.add(data)
+        return resp
+
+    if request.method == 'PUT':
+        return "cliente actualizado"   #update
+
+    if request.method == 'DELETE':
+        return "cliente eliminado"
+
+
+@app.route('/manejo_natural', methods= ['GET', 'POST','PUT','DELETE'])
+def manejo_natural():
+
     if request.method == 'GET':
-        return render_template("mostrar_clientes.html")
-    
+        
+        db = DB_cliente_natural()
+         
+        datos_usuario = db.get(cl_id) 
+
+        for atributo in datos_usuario[0]:
+            if (datos_usuario[0][atributo] == None): datos_usuario[0][atributo] = ''
+        
+        return jsonify(datos_usuario)
+
     if request.method == 'POST': 
        
-        db = DB_cliente_natural()         
-    
-        resp = db.getall()
+        db = DB_lugar()   
+
+        lu_codigo = db.getlastid()
+
+        direccion = {
+            'lu_codigo'     :   lu_codigo,
+            'lu_nombre'     :   request.form['inputdir'],        
+            'lu_tipo'       :   'DIRECCION',             
+            'fk_lugar'      :   request.form['selectparroquia'],         
+        }
+
+        id_direccion = db.add(direccion)
+
+
+        data = {
+            'cl_correo'     :    request.form['inputcorreo'],            #string    
+            'cl_cedula'     :int(request.form['inputcedula']),             #int 
+            'cl_rif'        :    request.form['inputrif'],               #int
+            
+            'cl_contrasena' :    request.form['inputcont'],         #string 
+            'cl_afiliacion' :    123,                                       #int
+            'cl_p_nombre'   :    request.form['inputpnombre'],             #string 
+            'cl_s_nombre'   :    request.form['inputsnombre'],               #string 
+            'cl_p_apellido' :    request.form['inputpapellido'],               #string 
+            'cl_s_apellido' :    request.form['inputsapellido'],                #string  
+            'fk_lugar'      :    None,    
+            'fk_tienda'     :    int(request.form['selecttienda']),
+        }
+
+        data['fk_lugar'] = id_direccion
+
+
+        for key in data.keys():
+            if (data[key] == '' or data[key] == ' '): data[key] = None
+
+        db = DB_cliente_natural()   
+        resp = db.add(data)
+
+
+        db = DB_telefono()   
+
+        telefono = {
+            'te_tipo'            :   request.form['tipotlf'],        
+            'te_numero'          :   int(request.form['inputtelefono']),             
+            'fk_cliente_natural' :   None,         
+        }
+
+        db.add(telefono) 
+
+
+        return resp
  
-        return jsonify(resp)
+    if request.method == 'PUT':
+        return "xd"
 
     if request.method == 'DELETE':
 
@@ -146,12 +241,16 @@ def mostrar():
         return resp
 
 
-@app.route('/lugares',methods=['POST','GET','PUT'])     #datatable falta update
+
+
+#### lugares #####
+
+
+@app.route('/lugares',methods=['POST','GET'])     #datatable falta update
 def lugares():
     
     if request.method == 'GET':
-
-        
+ 
         tipo = request.args.get('LUGAR')
         fk_lugar = request.args.get('FK_LUGAR')
 
@@ -169,109 +268,8 @@ def lugares():
         resp = db.getdir(fk_dir)
 
         return resp
-       
-@app.route('/tienda',methods=['POST','GET','PUT','DELETE'])
-def tienda():
-    
-    if request.method == 'GET':
-        return render_template("registro_tienda.html")
 
 
-    if request.method == 'POST':
-
-        db =DB_lugar()   
-
-        lu_codigo = db.getlastid()
-
-        direccion = {
-            'lu_codigo'     :   lu_codigo,
-            'lu_nombre'     :   request.form['inputdir'],        
-            'lu_tipo'       :   'DIRECCION',             
-            'fk_lugar'      :   request.form['selectparroquia'],         
-        }
-
-        id_direccion = db.add(direccion)
-
-        data = {
-
-            'ti_nombre'     :   request.form['inputtienda'],                        
-            'fk_lugar'      :   lu_codigo,
-        }
-
-        db = DB_tienda()  
-        resp = db.add(data)
-        return resp
-
-    if request.method == 'PUT':
-        return "cliente actualizado"   #update
-
-    if request.method == 'DELETE':
-        return "cliente eliminado"
- 
-
-@app.route('/manejo_tiendas',methods=['POST','GET','PUT'])     #datatable falta update
-def manejo_tiendas():
-    
-    
-    if request.method == 'GET':
-
-        db = DB_tienda()  
-        resp = db.getall()
-        return jsonify(resp)
-
-
-
-
-@app.route("/perfil_natural/<int:cl_id>", methods=['POST', 'GET','PUT'])    
-def perfil_natural(cl_id):
-   
-    if request.method == 'GET':                 
-        return render_template('perfil_natural.html', cl_id = cl_id)     
-
-    if request.method == 'PUT':
-        db = DB_cliente_natural()
-         
-        datos_usuario = db.get(cl_id) 
-
-        for atributo in datos_usuario[0]:
-            if (datos_usuario[0][atributo] == None): datos_usuario[0][atributo] = ''
-        
-        return jsonify(datos_usuario)
-
-    if request.method == 'POST':
-        return "xd"
-
-
-       
-
-    
-###########################
-
-
-@app.route('/perfil/<cl_nombre>')
-def perfil(cl_nombre):
-    return render_template("perfil_natural.html", cl_nombre = cl_nombre)    
-
-
-@app.route('/producto/<int:pr_id>')        #url generico
-def datos_producto(pr_id):
-    return "datos produto %s" %pr_id     #escribe en pantalla 
-
-
-@app.route('/carrito', methods= ['GET', 'POST'] )
-def carrito():
-
-    if request.method == 'GET':
-        
-        lista = ["Pantalones", "Pollo", "Caraotas"]
-
-
-        return render_template("carrito.html", lista = lista)   
-    
-
-    else:   return "ah bueno"
-
-#######################################
 
    
 

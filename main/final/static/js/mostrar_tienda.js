3ef380f13a10258e2de02d09e25b9282d94ff692
Maxell <alberto.manrique99@gmail.com>
2021-01-07 01:40:30 -0400

    ghjgj

--- main/final/static/js/mostrar_tienda.js
+++ main/final/static/js/mostrar_tienda.js
@@ -6,11 +6,11 @@ function alerta(mensaje){
 
 $(document).ready(function(){
 
-    $('#tabla_datable').DataTable({
+    $('#tabla_datatable').DataTable({
 
         scrollY:        '50vh',
         scrollCollapse:  true,
-        scrollX:         true,
+
 
         ajax: {
             url: '/mostrar/tiendas',
@@ -19,6 +19,7 @@ $(document).ready(function(){
         },
 
         columns:[
+            { data:      "ti_codigo"    ,   title:"CODIGO"},
             { data:      "ti_nombre" ,   title: "NOMBRE"},
             { data:      "fk_lugar"  ,   title: "DIRECCION"},
         ]
@@ -32,10 +33,58 @@ $(document).ready(function(){
         else {
             table.$('tr.selected').removeClass('selected');
             $(this).addClass('selected');
-        }
-        
+        }        
 
     });
+
+    $('#boton_eliminar').click( function () {
+        
+        var sel = table.row('.selected').data();
+
+        if (sel){
+          
+            var codigo = sel['ti_codigo'];
+
+            $.ajax({
+                
+                url:   '/manejo_tienda',
+                data:  codigo.toString(),
+                type: 'DELETE',
+                    
+                }).done(function(response){
+                    
+                    alerta(response['mensaje']);
+                    table.row('.selected').remove().draw( false );
+                    
+                }).fail(function(response){
+                    c_error('No se pudo acceder al servidor. Intente de nuevo mas tarde');
+                });
+
+        }
+        else alert('Debe seleccionar algo');
+            
+    });
+
+    $('#boton_añadir').click( function () {
+        
+        window.location.href =  "/registro/tienda"		
+
+    } );
+
+    $('#boton_modificar').click( function () {
+        
+        var sel = table.row('.selected').data();
+
+        if (sel){
+
+            var id = sel['cl_id'];
+            window.location.href =  '/natural/'+id;	
+
+        }
+
+        else alert('Debe seleccionar algo');
+    });
+
 
 
 

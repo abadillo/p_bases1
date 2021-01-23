51a38d89466860a776d7fc6576c51dd6b0a2da5c
antonio2924 <abadillo2924@gmail.com>
2021-01-23 14:52:26 -0400

    scrop

--- code/sql/DROP.sql
+++ code/sql/DROP.sql
@@ -1,47 +1,49 @@
+
+DROP TABLE metodo_pago_compra;
+DROP TABLE compra;
+DROP TABLE estatus_despacho;
+DROP TABLE carrito_producto;
+DROP TABLE carrito;
+/*CARRITO Y COMPRAS*/
+
+DROP TABLE pasillo;
+DROP TABLE almacen;
+DROP TABLE zona;
+DROP TABLE orden_reposicion;
+DROP TABLE estatus_reposicion;
+DROP TABLE descuento;
+DROP TABLE producto;
+DROP TABLE rubro;
+DROP TABLE marca;
+/*PRODUCTO , CARRITO, ORDENES*/
+
+DROP TABLE telefono;
+DROP TABLE persona_contacto;
+DROP TABLE proveedor;
+DROP TABLE usuario;
+/*CLIENTES , PROVEEDORES, EMPLEADOS Y PERSONAS CONTACTO*/
+
+DROP TABLE cotizacion;
+DROP TABLE moneda;
+DROP TABLE metodo_pago;
+DROP TABLE tipo_pago;
+DROP TABLE cliente;
+/*CLIENTES*/
+
+DROP TABLE vacaciones;
+DROP TABLE control_entrada;
+DROP TABLE beneficio_empleado;
+DROP TABLE beneficio;
+DROP TABLE horario_empleado;
+DROP TABLE horario;
+DROP TABLE empleado;
+/* EMPLEADOS */
+
+DROP TABLE privilegio_rol;
+DROP TABLE privilegio;
+DROP TABLE rol;
+DROP TABLE tienda;
+/* TIENDA Y  PRODUCTOS*/
+
 DROP TABLE lugar;
 
-/* TIENDA Y  PRODUCTOS*/
-DROP TABLE tienda;
-DROP TABLE rol;
-DROP TABLE privilegio;
-DROP TABLE privilegio_rol;
-
-/* EMPLEADOS */
-DROP TABLE empleado;
-DROP TABLE horario;
-DROP TABLE horario_empleado;
-DROP TABLE beneficio;
-DROP TABLE beneficio_empleado;
-DROP TABLE control_entrada;
-DROP TABLE vacaciones;
-
-/*CLIENTES*/
-DROP TABLE cliente;
-DROP TABLE tipo_pago;
-DROP TABLE metodo_pago;
-DROP TABLE moneda;
-DROP TABLE cotizacion;
-
-/*CLIENTES , PROVEEDORES, EMPLEADOS Y PERSONAS CONTACTO*/
-DROP TABLE usuario;
-DROP TABLE proveedor;
-DROP TABLE persona_contacto;
-DROP TABLE telefono;
-
-/*PRODUCTO , CARRITO, ORDENES*/
-DROP TABLE marca;
-DROP TABLE rubro;
-DROP TABLE producto;
-DROP TABLE descuento;
-DROP TABLE estatus_reposicion;
-DROP TABLE orden_reposicion;
-DROP TABLE zona;
-DROP TABLE almacen;
-DROP TABLE pasillo;
-
-/*CARRITO Y COMPRAS*/
-DROP TABLE carrito;
-DROP TABLE carrito_producto;
-DROP TABLE estatus_despacho;
-DROP TABLE compra;
-DROP TABLE metodo_pago_compra;
--- code/sql/_.sql
+++ code/sql/_.sql
@@ -1,13 +1,13 @@
-TABLE lugar;                       -- #LISTO   T: 1580 (1520 EMP, 59 D)
+TABLE lugar;                       -- #LISTO   T: 1580 (1520 EMP, 984 D ( 335P, 59T ,590 C ))  
 
 /* TIENDA Y  PRODUCTOS*/    
-TABLE tienda;                      -- #FALTA   T: 59      DIRECCIONES MAL
+TABLE tienda;                      -- #LISTO   T: 59      
 TABLE rol;                         -- #        T: 
 TABLE privilegio;                  -- #        T: 
 TABLE privilegio_rol;              -- #        T: 
 
 /* EMPLEADOS */
-TABLE empleado;                    -- #        T: 590   (531 update)
+TABLE empleado;                    -- #LISTO   T: 590   (531 update)
 TABLE horario;                     -- #LISTO   T: 14 (7 MAÑANA, 7 TARDE)
 TABLE horario_empleado;            -- #LISTO   T: 2124
 TABLE beneficio;                   -- #LISTO   T: 20
@@ -16,7 +16,7 @@ TABLE control_entrada;             -- #        T: 9558
 TABLE vacaciones;                  -- #        T: 
 
 /*CLIENTES*/
-TABLE cliente;                     -- #        T: 300/590 (80% NATURAL, 20% JURIDICO) ()
+TABLE cliente;                     -- #LISTO   T: 590 (80% NATURAL, 20% JURIDICO) ()
 TABLE tipo_pago;                   -- #LISTO   T: 5
 TABLE moneda;                      -- #LISTO   T: 5
 TABLE cotizacion;                  -- #LISTO   T: 8
@@ -24,8 +24,8 @@ TABLE metodo_pago;                 -- #LISTO   T: 178 (2 NATURAL, 1 JURIDICO X T
 
 /*CLIENTES , PROVEEDORES, EMPLEADOS 
 Y PERSONAS CONTACTO*/
-TABLE usuario;                     -- #        T: 
-TABLE proveedor;                   -- #        T: 21/336 (1 POR MUNICIPIO)
+TABLE usuario;                     -- #1180    T: 590 C, 590 E
+TABLE proveedor;                   -- #335     T: 335 (1 POR MUNICIPIO)
 TABLE persona_contacto;            -- #        T: 
 TABLE telefono;                    -- #        T: 
 
@@ -45,5 +45,5 @@ TABLE pasillo;                     -- #LISTO   T: 6490 (20 X PRODUCTO POR TIENDA
 TABLE carrito;                     -- #LISTO   T: 413
 TABLE carrito_producto;            -- #LISTO   T: 1416
 TABLE estatus_despacho;            -- #LISTO   T: 3
-TABLE compra;                      -- #        T: 413 (295 WEB / 118 FISICAS)  5 WEB, 2 FISICA POR TIENDA       ))))
-TABLE metodo_pago_compra;          -- #        T: 
+TABLE compra;                      -- #LISTO   T: 413 (295 WEB / 118 FISICAS)  5 WEB, 2 FISICA POR TIENDA       ))))
+TABLE metodo_pago_compra;          -- #        T:       FALTAN ASIGNAR PUNTOS A CLEINTES Y RESTAR DE INVENTARIO .. HACER ESO CON UPDATE

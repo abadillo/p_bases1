b954f6ff975c957e2eac8a2cb2662f3f721821f0
antonio2924 <abadillo2924@gmail.com>
2021-01-25 23:31:27 -0400

    scriptv2

---
+++ data/SCRIPTS/5.0_horario_beneficio.sql
@@ -0,0 +1,53 @@
+﻿--Lunes
+INSERT INTO horario (ho_descripcion, ho_dia, ho_hora_entrada, ho_hora_salida) VALUES ('Lunes-Turno mañana','LUNES' ,'07:00' ,'12:00');
+INSERT INTO horario (ho_descripcion, ho_dia, ho_hora_entrada, ho_hora_salida) VALUES ('Lunes-Turno tarde','LUNES' ,'12:00' ,'7:00');
+
+--Martes
+INSERT INTO horario (ho_descripcion, ho_dia, ho_hora_entrada, ho_hora_salida) VALUES ('Martes-Turno mañana','MARTES' ,'07:00' ,'12:00');
+INSERT INTO horario (ho_descripcion, ho_dia, ho_hora_entrada, ho_hora_salida) VALUES ('Martes-Turno tarde','MARTES' ,'12:00' ,'7:00');
+
+--Miercoles
+INSERT INTO horario (ho_descripcion, ho_dia, ho_hora_entrada, ho_hora_salida) VALUES ('Miercoles-Turno mañana','MIERCOLES' ,'07:00' ,'12:00');
+INSERT INTO horario (ho_descripcion, ho_dia, ho_hora_entrada, ho_hora_salida) VALUES ('Miercoles-Turno tarde','MIERCOLES' ,'12:00' ,'7:00');
+
+--Jueves
+INSERT INTO horario (ho_descripcion, ho_dia, ho_hora_entrada, ho_hora_salida) VALUES ('Jueves-Turno mañana','JUEVES' ,'07:00' ,'12:00');
+INSERT INTO horario (ho_descripcion, ho_dia, ho_hora_entrada, ho_hora_salida) VALUES ('Jueves-Turno tarde','JUEVES' ,'12:00' ,'7:00');
+
+--Viernes
+INSERT INTO horario (ho_descripcion, ho_dia, ho_hora_entrada, ho_hora_salida) VALUES ('Viernes-Turno mañana','VIERNES' ,'07:00' ,'12:00');
+INSERT INTO horario (ho_descripcion, ho_dia, ho_hora_entrada, ho_hora_salida) VALUES ('Viernes-Turno tarde','VIERNES' ,'12:00' ,'7:00');
+
+--Sabado
+INSERT INTO horario (ho_descripcion, ho_dia, ho_hora_entrada, ho_hora_salida) VALUES ('Sabado-Turno mañana','SABADO' ,'07:00' ,'12:00');
+INSERT INTO horario (ho_descripcion, ho_dia, ho_hora_entrada, ho_hora_salida) VALUES ('Sabado-Turno tarde','SABADO' ,'12:00' ,'7:00');
+
+--Domingo
+INSERT INTO horario (ho_descripcion, ho_dia, ho_hora_entrada, ho_hora_salida) VALUES ('Domingo-Turno mañana','DOMINGO' ,'07:00' ,'12:00');
+INSERT INTO horario (ho_descripcion, ho_dia, ho_hora_entrada, ho_hora_salida) VALUES ('Domingo-Turno tarde','DOMINGO' ,'12:00' ,'7:00');
+
+
+
+
+
+INSERT INTO beneficio (be_nombre) VALUES ('Seguro Medico');
+INSERT INTO beneficio (be_nombre) VALUES ('Jubilacion');
+INSERT INTO beneficio (be_nombre) VALUES ('Gastos de Transporte');
+INSERT INTO beneficio (be_nombre) VALUES ('Seguro de Vida');
+INSERT INTO beneficio (be_nombre) VALUES ('Seguro de Discapacidad');
+INSERT INTO beneficio (be_nombre) VALUES ('Reembolso de Matriculas');
+INSERT INTO beneficio (be_nombre) VALUES ('Descuento en Gimnasio');
+INSERT INTO beneficio (be_nombre) VALUES ('Estacionamiento Gratuito');
+INSERT INTO beneficio (be_nombre) VALUES ('Vacaciones');
+INSERT INTO beneficio (be_nombre) VALUES ('Pensiones');
+INSERT INTO beneficio (be_nombre) VALUES ('Pago de Horas Extras');
+INSERT INTO beneficio (be_nombre) VALUES ('Pago de Horas Nocturnas');
+INSERT INTO beneficio (be_nombre) VALUES ('Teletrabajo');
+INSERT INTO beneficio (be_nombre) VALUES ('Pago de Guardería');
+INSERT INTO beneficio (be_nombre) VALUES ('Extensión de Baja Maternal/Paternal');
+INSERT INTO beneficio (be_nombre) VALUES ('Formación Laboral');
+INSERT INTO beneficio (be_nombre) VALUES ('Utilidades/Aguinaldos');
+INSERT INTO beneficio (be_nombre) VALUES ('Fondo de Ahorro');
+INSERT INTO beneficio (be_nombre) VALUES ('Bonos de Productividad');
+INSERT INTO beneficio (be_nombre) VALUES ('Comedor Gratuito');
+
